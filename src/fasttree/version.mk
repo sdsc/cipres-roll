e026f0791e936687f5b39e773db1bc02aa926800
Jerry Greenberg <jpg@sdsc.edu>
2019-01-08 18:37:51 -0800

    APS-974 fasttree 2.1.10

--- src/fasttree/version.mk
+++ src/fasttree/version.mk
@@ -4,14 +4,14 @@ endif
 COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
 
 NAME           = sdsc-fasttree
-VERSION        = 2.1.8
-RELEASE        = 3
+VERSION        = 2.1.10
+RELEASE        = 0
 
 SRC_SUBDIR     = fasttree
 
 SOURCE_NAME    = fasttree
 SOURCE_SUFFIX  = tar.gz
-SOURCE_VERSION = 2.1.8
+SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
